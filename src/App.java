29ea7f18c5d3e95d8f6a0f7fd57b96c523baf294
Gearz <shanegeary1994@gmail.com>
2021-10-23 12:09:27 -0600

    Added String and char variable data types

--- src/App.java
+++ src/App.java
@@ -1,9 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        String pupName = "Gracie";
         int pupAge = 2;
         double pupFluff = 8.2;
         boolean pupIsGoof = true;
+        char pupFirstInitial = pupName.charAt(0);
 
+        System.out.println(pupName);
+        System.out.println(pupFirstInitial);
         System.out.println(pupAge);
         System.out.println(pupFluff);
         System.out.println(pupIsGoof);
