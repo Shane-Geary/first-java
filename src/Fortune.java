b34de91915992ca208f7178d44873376da5247cd
Gearz <shanegeary1994@gmail.com>
2021-10-23 16:18:38 -0600

    Added conditional

--- src/Fortune.java
+++ src/Fortune.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
 public class Fortune {
-    public static void fortune(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Pick a numberbetween 1 and 10");
         Scanner scanner = new Scanner(System.in);
 
         int inputtedNum = scanner.nextInt();
+
+        if(inputtedNum <= 5) {
+            System.out.println("Lots of coffee in your future");
+        } else {
+            System.out.println("Lots of guacamole in your future");
+        }
     }
 }
