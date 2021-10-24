ff0e6ea9ae88428aad278c8975ca2af6eb9bfd3e
Gearz <shanegeary1994@gmail.com>
2021-10-24 09:06:27 -0600

    Building out first Java function

--- src/TeaTime.java
+++ src/TeaTime.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class TeaTime {
 
     public static void announceDeveloperTeatime() {
-        
+        System.out.println("Waiting for Developer Tea Time...");
+        System.out.println("Type in a word and press enter to start developer tea time:");
+
+        Scanner input = new Scanner(System.in);
     }
     public static void main(String[] args) {
 
